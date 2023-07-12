c56c2e23807a9c64f7e09866f0018531fc67aea6
faveee <abatangift11@gmail.com>
2023-07-12 16:34:49 +0100

    New Feature
    description

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+        <div className="w-full h-full">
+      <div
+        className="w-screen h-[180px]"
+        style={{
+          backgroundImage: `url(/pattern.png)`,
+        }}>
+      </div>
+      <p className=''>
+        Hello
+      </p>
+    </div>
+  )
+}
+
+export default App
