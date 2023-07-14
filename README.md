8dd7ad4369252e7962b5ffadd541b71ed6d18e52
faveee <abatangift11@gmail.com>
2023-07-14 16:25:07 +0100

    Added leaflet

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ This is a solution to the [IP address tracker challenge on Frontend Mentor](http
 ### The challenge
 
 Users should be able to:
-
+  
 - View the optimal layout for each page depending on their device's screen size
 - See hover states for all interactive elements on the page
 - See their own IP address on the map on the initial page load
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,74 @@
 
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
-function App() {
+const App = () => {
+  const [address, setAddress] = useState([])
+
+// const address = {
+//   apiKey: process.env.PUBLIC_URL
+// }
+// address[ip, setIp] = useState()
+// address[locationData, setLocationData] = useState()
+// address[timezoneData, setTimezoneData] = useState()
+// address[ispData, setIspData] = useState()
+
+  const fetchData = () => {
+    fetch("https://geo.ipify.org/api/v2/country?apiKey=at_7SeAmbKbP4axsH6NqfjiCVnMudlXw&ipAddress=8.8.8.8")
+   .then(response => {
+    return response.json()
+   })
+
+   .then(data => {
+    setAddress(data)
+   })
+
+  }
+  
+  useEffect(() =>{
+    fetchData()
+  }, [])
+
+  
 
   return (
         <div className="w-full h-full">
       <div
-        className="w-screen h-[180px]"
+        className="w-screen h-[220px]"
         style={{
           backgroundImage: `url(/pattern.png)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
         }}>
-      </div>
-      <p className=''>
-        Hello
-      </p>
+          <div className='flex flex-col items-center gap-10'>
+          <p className="text-white text-xl pt-8">IP Address Tracker</p>
+            <input className='w-96 bg-white p-3  rounded-l-lg text-sm text-cyan-700 ' type="text" placeholder='Search for any IP address or domain' required />
+            <button className='absolute top-12 right-14 w-4 bg-black p-3 text-white hover:cursor-pointer' type='submit'>x</button>
+            </div>
+
+            
+            {/* IP Address
+            Location
+            Timezone
+            Isp */}
+          </div>
+
+          {/* California lat and long */}
+          <MapContainer center={[6.524379, 3.379206]} zoom={12} >
+          <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+                {/* {address.map(address => (
+                <Marker key={address.id} position={[address.ip, address.location, address.timezone, address.isp]}
+                /> */}
+                ))}
+          </MapContainer>
     </div>
-  )
+  );
 }
 
 export default App
+
+
