229dc2ec23cbbf75311680d074cf0e80082a0705
faveee <abatangift11@gmail.com>
2023-07-17 11:01:46 +0100

    Updated Search button UI

--- src/App.jsx
+++ src/App.jsx
@@ -33,19 +33,23 @@ const App = () => {
   
 
   return (
-        <div className="w-full h-full">
+        <div className="w-screen h-screen">
       <div
-        className="w-screen h-[220px]"
+        className="w-screen h-[180px]"
         style={{
           backgroundImage: `url(/pattern.png)`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover'
         }}>
-          <div className='flex flex-col items-center gap-10'>
+          <div className='flex flex-col items-center gap-9'>
           <p className="text-white text-xl pt-8">IP Address Tracker</p>
-            <input className='w-96 bg-white p-3  rounded-l-lg text-sm text-cyan-700 ' type="text" placeholder='Search for any IP address or domain' required />
-            <button className='absolute top-12 right-14 w-4 bg-black p-3 text-white hover:cursor-pointer' type='submit'>x</button>
-            </div>
+          </div>
+          <div className='flex pt-8 justify-center'>
+          <input type="search" className="w-96 p-2.5 z-20 text-sm text-gray-900 bg-gray-50 rounded-l-lg border dark:bg-gray-700 dark:border-gray-600 dark:placeholder-pink-400 dark:text-white dark:focus:border-blue-500" placeholder="Search for any IP address or domain" required/>
+            <button type="submit" className="p-2 text-sm bg-gray-900 rounded-r-lg border border-gray-900 hover:bg-red-500 focus:ring-4 focus:outline-none"> <svg fill="#ffffff" width="19px" height="24px" viewBox="-15.36 -15.36 542.72 542.72" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.00512"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></g></svg>
+  
+  </button>
+</div>
 
             
             {/* IP Address
@@ -55,15 +59,15 @@ const App = () => {
           </div>
 
           {/* California lat and long */}
-          <MapContainer center={[6.524379, 3.379206]} zoom={12} >
+          <MapContainer center={[38.296452, -76.508827]} zoom={12} >
           <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
                 {/* {address.map(address => (
                 <Marker key={address.id} position={[address.ip, address.location, address.timezone, address.isp]}
-                /> */}
-                ))}
+                />
+                ))} */}
           </MapContainer>
     </div>
   );
